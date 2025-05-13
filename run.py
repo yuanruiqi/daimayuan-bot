0700c8f7e097b8b643ba7795a734a68c71955ebe
yuanruiqi <99478037+yuanruiqi@users.noreply.github.com>
2025-05-13 21:53:09 +0800

    Remove some data files.

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ import down
 import ren
 
 def run(a, b, c):
+    if not os.path.exists('cache.json'):
+        with open('cache.json', 'w') as f:
+            f.write('{}')
     down.run(a, b, c)
     os.system('./bin')
     ren.run()
